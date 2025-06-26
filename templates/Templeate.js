7890faccd5324a6f6ed01a208252dec6937c5360
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-27 00:24:10 +0530

    added app template

--- templates/Templeate.js
+++ templates/Templeate.js
@@ -14,3 +14,20 @@ const ${Route} = () => {
 export default ${Route}    
 `)
 }
+
+export const AppTemplate = () => {
+  return (
+    `import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;`
+  )
+}
