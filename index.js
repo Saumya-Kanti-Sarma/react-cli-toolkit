4da2da988880dd9bb507227a016a7a25ba9916e9
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-27 00:23:40 +0530

    added package checker

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
+import checkPackage from "./utils/checkPackage.js";
 import createRoute from "./utils/createRoute.js";
 
 const input = process.argv.splice(2).join(" ")
 if (!input) {
   console.log("this is react-cli-toolkit");
 }
-
-// create routes
-createRoute(input);
+else {
+  // check if react router dom is installed:
+  checkPackage();
+  // create routes
+  createRoute(input);
+}
---
+++ utils/checkPackage.js
@@ -0,0 +1,11 @@
+import { exec } from "child_process";
+import fs from "fs";
+import { AppTemplate } from "../templates/Templeate.js";
+
+export default function checkPackage() {
+  const readPackage = fs.readFileSync("package.json", "utf-8");
+  if (!readPackage.includes("react-router-dom")) {
+    exec("npm install react-router-dom");
+  };
+  fs.writeFileSync('src/App.jsx', AppTemplate());
+};
