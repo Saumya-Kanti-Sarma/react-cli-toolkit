7788d97239ee54662e321f54135761a4f189605f
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-26 23:06:21 +0530

    mini changes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 import fs from "fs";
+import path from "path";
+import { ReactTemplate } from "./templates/Templeate.js";
 
 const input = process.argv.splice(2).join(" ")
 
 if (!input) {
   console.log("this is react-cli-toolkit");
-
 }
+// create routes
+function createRoutes() {
+  const pagesDir = path.join("src", "pages");
+  fs.mkdirSync(pagesDir, { recursive: true });
+  const FileName = input.charAt(0).toUpperCase() + input.slice(1);
+  const filePath = path.join(pagesDir, `${FileName}.jsx`);
+  fs.writeFileSync(filePath, `${ReactTemplate(`${FileName}`)}`);
+  console.log(`${FileName} route created at ${filePath}`);
+}
+createRoutes();
---
+++ templates/Templeate.js
@@ -0,0 +1,14 @@
+export const ReactTemplate = (Route) => {
+  return (`
+import React from 'react';
+const ${Route} = () => {
+  return (
+    <>
+      route: ${Route}
+    </>
+  )
+}
+
+export default ${Route}    
+`)
+}
